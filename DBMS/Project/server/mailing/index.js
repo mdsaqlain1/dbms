ab04fb39b363509a8fb0f5a746a2cd6b3c1bf4f9
Mohammed Saqlain <md.saqlain3100@gmail.com>
2024-12-14 14:54:46 +0530

    Updated the folders

--- DBMS/Project/server/mailing/index.js
+++ DBMS/Project/server/mailing/index.js
@@ -8,8 +8,8 @@ router.post('/sendemail', async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'example@gmail.com',
-      pass: 'your password'
+      user: 'mohammedsaqlain202003@gmail.com',
+      pass: 'hxwj xmol idem kcml'
     }
   });
 
--- React-Landing-Page-Template/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    //...
-    resolve: {
-      fallback: {
-        "stream": require.resolve("stream-browserify")
-      }
-    }
- };
